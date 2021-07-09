3322dc83b1bf09abaa71ad9597e6e510b3a1783b
Mayank Gandhi <mayankgandhi50@gmail.com>
2021-07-09 13:54:59 +0530

    Initial Commit

---
+++ WhatsNew/WhatsNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsNewApp.swift
+//  WhatsNew
+//
+//  Created by Mayank Gandhi on 09/07/21.
+//
+
+import SwiftUI
+
+@main
+struct WhatsNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
